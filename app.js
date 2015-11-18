e5b9adc25178ed3990f799a0e368d98858dab6ff
manavmanocha <manav.manocha@comprotechnologies.com>
2015-11-18 19:13:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    app     = express(),
+    PouchDB = require('pouchdb'),
+    cors = require('cors');
+
+var corsOptions = {
+  origin: 'http://localhost:8080',
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+var InMemPouchDB = PouchDB.defaults({db: require('memdown')});
+app.use('/testdb', require('express-pouchdb')(InMemPouchDB, {
+  mode: 'fullCouchDB'
+}));
+
+var port = 5984;
+app.listen(port, function () {  
+  console.log('PouchDB Test running on ' + port + " port.");  
+});
