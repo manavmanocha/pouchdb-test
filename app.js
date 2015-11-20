48b4927a01779363e40657a30ac6b9d7bed898e8
manavmanocha <manav.manocha@comprotechnologies.com>
2015-11-20 16:13:57 +0530

    Write console logs to file also

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var express = require('express'),
     cors = require('cors'),
     bodyParser = require('body-parser');
 
+
+var fs = require('fs');
+var util = require('util');
+
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};    
+
 var requestLogger = function (req, res, next){
   var log =  req.method + " "
   			+ req.url + "\n"
